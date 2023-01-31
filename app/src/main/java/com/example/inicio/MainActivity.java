42c8e86fc3efa3dea578a8f36e50435e86cbadb9
Alejandro Garcia <agarciachacon51z@gmail.com>
2023-01-31 10:45:43 +0100

    Primer Commit and push

---
+++ app/src/main/java/com/example/inicio/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button modiEquipos;
+    Button modiPartidos;
+    Button verPartido;
+    Button salir;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        modiEquipos= findViewById(R.id.modificarEqui);
+        modiPartidos= findViewById(R.id.modificarPar);
+        verPartido= findViewById(R.id.verPartidos);
+        salir= findViewById(R.id.salirAplicacion);
+
+        modiPartidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                modificiarPartidos();
+            }
+        });
+
+        modiEquipos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                modificarEquipos();
+            }
+        });
+
+        verPartido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verPartidos();
+            }
+        });
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+
+    public void verPartidos() {
+        Intent i= new Intent(this, Ver_Resultados.class);
+        startActivity(i);
+    }
+
+    public void modificarEquipos() {
+        Intent i= new Intent(this, Modificar_Equipo.class);
+        startActivity(i);
+    }
+
+    public void modificiarPartidos() {
+        Intent i= new Intent(this, Moficar_Partidos.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/inicio/Ver_Resultados.java
@@ -0,0 +1,14 @@
+package com.example.inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ver_Resultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_resultados);
+    }
+}
